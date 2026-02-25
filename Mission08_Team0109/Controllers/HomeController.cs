92b429341828941bb86735a2135b51813dce146b
peter <peter@LAPTOP17>
2026-02-25 09:56:01 -0700

    started building controller

--- Mission08_Team0109/Controllers/HomeController.cs
+++ Mission08_Team0109/Controllers/HomeController.cs
@@ -1,15 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Mission08_Team0109.Controllers
 {
     public class HomeController : Controller
     {
+        // Renders the index and passes in all tasks
         public IActionResult Index()
         {
-            return View();
+            var tasks = _context.Tasks
+                .Where(x => x.Completed == false).ToList();
+            return View(tasks);
         }
+
+        // Gets called to allow the user to add a task
+        [HttpGet] 
+        public IActionResult AddEditTask()
+        {
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            return View("AddEditTask", new TaskItem());
+        }
+
+
+        [HttpPost]
+        public IActionResult AddEditTask(TaskItem response)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.addTask(response); // Add record to database
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+                return View(response);
+            }
+        }
+        
 
         
     }
